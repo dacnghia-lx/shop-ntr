709e046c5db8330a1355c3b6125beb3ea9bce5ef
nghia <orange25k5@gmail.com>
2025-02-21 14:21:54 +0700

    ...

--- js/firebase.js
+++ js/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDMGrExQqAlVChfbGVuvjEJFUD9nnXXtyg",
-  authDomain: "test-2037f.firebaseapp.com",
-  projectId: "test-2037f",
-  storageBucket: "test-2037f.firebasestorage.app",
-  messagingSenderId: "899164226404",
-  appId: "1:899164226404:web:30e9f7c02575c2a65fb071",
-  measurementId: "G-EQKCBBWL6V"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
